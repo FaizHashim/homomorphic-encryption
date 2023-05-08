2b5f95097db7ee4091f7ac784756a2e5d6ee627c
Faiz <faizhashim2003@gmail.com>
2023-05-08 22:54:57 +0530

    Updated files

--- Assignment 1/cloud_server/cs.py
+++ Assignment 1/cloud_server/cs.py
@@ -27,6 +27,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         conn, addr = s.accept()
 
         with conn:
+            print(f"Received connection from {addr}")
             while True:
                 data = conn.recv(1024)
                 if not data:
